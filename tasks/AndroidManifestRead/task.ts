e9cabdfe7a8c1b3663d9cc481a1a89f024f59a2c
Tom Curran <tom@tomcurran.org>
2018-02-03 13:39:07 +0000

    added android manifest read task

--- tasks/AndroidManifestRead/task.ts
+++ tasks/AndroidManifestRead/task.ts
@@ -1,22 +1,82 @@
 import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 import fs = require('fs');
+import sam = require('samchon-framework');
 import { isNullOrUndefined } from 'util';
-
 
 async function run() {
     try {
         let tool: trm.ToolRunner;
 
-        let message: string = tl.getInput("key_from_task");
+        let sourcePath: string = tl.getPathInput("sourcePath", true, true);
+        console.log("Provided manifest path: " + sourcePath);
 
-        if(isNullOrUndefined(message))
+        let xmlString: string = fs.readFileSync(sourcePath, 'utf8');
+        let xml: sam.library.XML = new sam.library.XML(xmlString);
+
+        if (xml.hasProperty("android:versionCode"))
         {
-            message = "No message defined";
+            let versionCode = xml.getProperty("android:versionCode");
+            tl.setVariable("androidManfiestVersionCode", versionCode);
+            console.log("Version code: " + versionCode + " -> Saved to androidManfiestVersionCode environment variable.");
+        }
+        else
+        {
+            console.log("No version code found in manifest");
         }
 
-        
-        console.log(message);
+        if (xml.hasProperty("android:versionName"))
+        {
+            let versionName = xml.getProperty("android:versionName");
+            tl.setVariable("androidManfiestVersionName", versionName);
+            console.log("Version name: " + versionName + " -> Saved to androidManfiestVersionName environment variable.");
+        }
+        else
+        {
+            console.log("No version name found in manifest");
+        }
+
+        if (xml.hasProperty("package"))
+        {
+            let packageName = xml.getProperty("package");
+            tl.setVariable("androidManfiestPackage", packageName);
+            console.log("Package: " + packageName + " -> Saved to androidManfiestPackage environment variable.");
+        }
+        else
+        {
+            console.log("No package found in manifest");
+        }
+
+        if (xml.has("uses-sdk"))
+        {
+            let usesSdk = xml.get("uses-sdk").at(0);
+
+            if (usesSdk.hasProperty("android:minSdkVersion"))
+            {
+                let minSdkVersion = usesSdk.getProperty("android:minSdkVersion");
+                tl.setVariable("androidManfiestMinSdkVersion", minSdkVersion);
+                console.log("Minimum SDK version: " + minSdkVersion + " -> Saved to androidManfiestMinSdkVersion environment variable.");
+            }
+            else
+            {
+                console.log("No minimum SDK version found in manifest");
+            }
+
+            if (usesSdk.hasProperty("android:targetSdkVersion"))
+            {
+                let targetSdkVersion = usesSdk.getProperty("android:targetSdkVersion");
+                tl.setVariable("androidManfiestTargetSdkVersion", targetSdkVersion);
+                console.log("Target SDK version: " + targetSdkVersion + " -> Saved to androidManfiestTargetSdkVersion environment variable.");
+            }
+            else
+            {
+                console.log("No target SDK version found in manifest");
+            }
+        }
+        else
+        {
+            console.log("No uses sdk tag found in manifest");
+        }
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
