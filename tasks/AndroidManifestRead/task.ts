9882e1a9292f692e675516db5d5d9320f1e5aae7
Tom Curran <tom@tomcurran.org>
2018-02-06 23:58:54 +0000

    Android Manifest Read - removed unused dependency "util"

--- tasks/AndroidManifestRead/task.ts
+++ tasks/AndroidManifestRead/task.ts
@@ -2,7 +2,6 @@ import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 import fs = require('fs');
 import sam = require('samchon-framework');
-import { isNullOrUndefined } from 'util';
 
 async function run() {
     try {
