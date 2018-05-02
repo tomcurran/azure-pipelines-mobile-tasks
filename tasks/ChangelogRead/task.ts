73e09500231430eec5f5440720d6be8dc5864ccd
Tom Curran <tom@tomcurran.org>
2018-05-02 21:48:24 +0100

    fixed changelog regex by removing named groups (not yet supported)

--- tasks/ChangelogRead/task.ts
+++ tasks/ChangelogRead/task.ts
@@ -11,16 +11,26 @@ async function run() {
 
         let changelog: string = fs.readFileSync(sourcePath, 'utf8');
 
-        let pattern = new RegExp("\[(?<version>(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>[0-9A-Za-z\-\.]+))?)\]\s+\-\s+(?<date>.*)(?<notes>[^#]*)");
+        // pattern with named groups: /\[(?<version>(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>[0-9A-Za-z\-\.]+))?)\]\s+\-\s+(?<date>.*)(?<notes>[^#]*)/;
+        let pattern = /\[((\d+)(\.(\d+))?(\.(\d+))?(\-([0-9A-Za-z\-\.]+))?(\+([0-9A-Za-z\-\.]+))?)\]\s+\-\s+(.*)([^#]*)/;
         if (!pattern.test(changelog)) {
             throw new Error("No match found for version string in changelog");
         }
         else
         {
             let matches = pattern.exec(changelog);
-            let changlelogVersion: string = matches[0];
+
+            let changlelogVersion: string = matches[1];
             tl.setVariable("ChanglelogVersion", changlelogVersion);
             console.log("Changlelog Version: " + changlelogVersion + " -> Saved to ChanglelogVersion environment variable.");
+            
+            let changlelogChanges: string = matches[12].trim();
+            tl.setVariable("ChanglelogChanges", changlelogChanges);
+            console.log("Changlelog Changes: " + changlelogChanges + " -> Saved to ChanglelogChanges environment variable.");
+
+            let changlelogDate: string = matches[11];
+            tl.setVariable("ChanglelogDate", changlelogDate);
+            console.log("Changlelog Date: " + changlelogDate + " -> Saved to ChanglelogDate environment variable.");
         }
     }
     catch (err) {
